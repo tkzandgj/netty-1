a63f60cdacab750a9b80f3d9e6e378435cb618b8
tukangzheng <tukangzheng@gomeplus.com>
2017-10-12 17:21:50 +0800

    添加logger

--- Server/src/main/java/com/yao/NettyServerHandler.java
+++ Server/src/main/java/com/yao/NettyServerHandler.java
@@ -5,36 +5,63 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.util.ReferenceCountUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by yaozb on 15-4-11.
  */
 public class NettyServerHandler extends SimpleChannelInboundHandler<BaseMsg> {
+    private static final Logger logger = LoggerFactory.getLogger(NettyServerHandler.class);
+
+    /**
+     * Channel处于活动状态（已经连接到它的远程节点）。它现在可以接收和发送数据了
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        logger.info("client connected server established......");
+    }
+
+    /**
+     * Channel没有连接到远程节点的时候调用
+     * @param ctx
+     * @throws Exception
+     */
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
         NettyChannelMap.remove((SocketChannel)ctx.channel());
     }
-    @Override
-    protected void messageReceived(ChannelHandlerContext channelHandlerContext, BaseMsg baseMsg) throws Exception {
 
+    /**
+     * 接收客户端发送的消息
+     * @param channelHandlerContext
+     * @param baseMsg
+     * @throws Exception
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, BaseMsg baseMsg) throws Exception {
+        logger.info("读取客户端发送的消息......");
         if(MsgType.LOGIN.equals(baseMsg.getType())){
             LoginMsg loginMsg=(LoginMsg)baseMsg;
-            if("robin".equals(loginMsg.getUserName())&&"yao".equals(loginMsg.getPassword())){
+            if("admin".equals(loginMsg.getUserName())&&"admin123".equals(loginMsg.getPassword())){
                 //登录成功,把channel存到服务端的map中
                 NettyChannelMap.add(loginMsg.getClientId(),(SocketChannel)channelHandlerContext.channel());
-                System.out.println("client"+loginMsg.getClientId()+" 登录成功");
+                logger.info("client"+loginMsg.getClientId()+" 登录成功");
             }
         }else{
             if(NettyChannelMap.get(baseMsg.getClientId())==null){
-                    //说明未登录，或者连接断了，服务器向客户端发起登录请求，让客户端重新登录
-                    LoginMsg loginMsg=new LoginMsg();
-                    channelHandlerContext.channel().writeAndFlush(loginMsg);
+                //说明未登录，或者连接断了，服务器向客户端发起登录请求，让客户端重新登录
+                LoginMsg loginMsg=new LoginMsg();
+                channelHandlerContext.channel().writeAndFlush(loginMsg);
             }
         }
         switch (baseMsg.getType()){
             case PING:{
                 PingMsg pingMsg=(PingMsg)baseMsg;
                 PingMsg replyPing=new PingMsg();
+                // 获取连接
                 NettyChannelMap.get(pingMsg.getClientId()).writeAndFlush(replyPing);
             }break;
             case ASK:{
@@ -51,7 +78,7 @@ public class NettyServerHandler extends SimpleChannelInboundHandler<BaseMsg> {
                 //收到客户端回复
                 ReplyMsg replyMsg=(ReplyMsg)baseMsg;
                 ReplyClientBody clientBody=(ReplyClientBody)replyMsg.getBody();
-                System.out.println("receive client msg: "+clientBody.getClientInfo());
+                logger.info("receive client msg: "+clientBody.getClientInfo());
             }break;
             default:break;
         }
