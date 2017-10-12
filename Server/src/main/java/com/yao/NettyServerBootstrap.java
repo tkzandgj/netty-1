5c176f60783c28378164fdc78c68cb239ffb0f90
tukangzheng <tukangzheng@gomeplus.com>
2017-10-12 17:21:24 +0800

    添加logger

--- Server/src/main/java/com/yao/NettyServerBootstrap.java
+++ Server/src/main/java/com/yao/NettyServerBootstrap.java
@@ -9,6 +9,8 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.serialization.ClassResolvers;
 import io.netty.handler.codec.serialization.ObjectDecoder;
 import io.netty.handler.codec.serialization.ObjectEncoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,6 +18,9 @@ import java.util.concurrent.TimeUnit;
  * Created by yaozb on 15-4-11.
  */
 public class NettyServerBootstrap {
+
+    private static final Logger logger = LoggerFactory.getLogger(NettyServerBootstrap.class);
+
     private int port;
     private SocketChannel socketChannel;
     public NettyServerBootstrap(int port) throws InterruptedException {
@@ -31,7 +36,7 @@ public class NettyServerBootstrap {
         bootstrap.channel(NioServerSocketChannel.class);
         bootstrap.option(ChannelOption.SO_BACKLOG, 128);
         bootstrap.option(ChannelOption.TCP_NODELAY, true);
-        bootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
+        bootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);  // TCP层面的连接
         bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
             @Override
             protected void initChannel(SocketChannel socketChannel) throws Exception {
@@ -41,9 +46,10 @@ public class NettyServerBootstrap {
                 p.addLast(new NettyServerHandler());
             }
         });
+        // 异步连接   做一个回调   判断客户端是否连接成功
         ChannelFuture f= bootstrap.bind(port).sync();
         if(f.isSuccess()){
-            System.out.println("server start---------------");
+            logger.info("server start---------------");
         }
     }
     public static void main(String []args) throws InterruptedException {
